b8a78dcba9524341d1568f8c7e8980ad913eabdc
Sajith Perera <sajith.perera@gmail.com>
2013-02-13 08:38:32 +0000

    override sub for building oligo_target_regions_dir attribute was in wrong action

--- lib/DesignCreate/Action/RunAOS.pm
+++ lib/DesignCreate/Action/RunAOS.pm
@@ -27,10 +27,14 @@ use strict;
 use warnings FATAL => 'all';
 
 use Moose;
+use Const::Fast;
 use namespace::autoclean;
 
 extends qw( DesignCreate::Action );
 with 'DesignCreate::CmdRole::RunAOS';
+
+#TODO move this
+const my $DEFAULT_OLIGO_TARGET_REGIONS_DIR_NAME => 'oligo_target_regions';
 
 sub execute {
     my ( $self, $opts, $args ) = @_;
@@ -39,6 +43,20 @@ sub execute {
 
     return;
 }
+
+# if running command by itself we want to check the target regions dir exists
+# default is to delete and re-create folder
+override _build_oligo_target_regions_dir => sub {
+    my $self = shift;
+
+    my $target_regions_dir = $self->dir->subdir( $DEFAULT_OLIGO_TARGET_REGIONS_DIR_NAME );
+    unless ( $self->dir->contains( $target_regions_dir ) ) {
+        $self->log->logdie( "Can't find aos output dir: "
+                           . $target_regions_dir->stringify );
+    }
+
+    return $target_regions_dir->absolute;
+};
 
 __PACKAGE__->meta->make_immutable;
 
