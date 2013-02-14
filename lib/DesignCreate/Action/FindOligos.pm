529088d9894ee89c03b4f7d568ee899339b5ed4e
Sajith Perera <sajith.perera@gmail.com>
2013-02-14 08:30:17 +0000

    must override build method for oligo_target_regions_dir attribute in find-oligos command to stop it deleting the required folder when command run on its own

--- lib/DesignCreate/Action/FindOligos.pm
+++ lib/DesignCreate/Action/FindOligos.pm
@@ -15,10 +15,15 @@ use strict;
 use warnings FATAL => 'all';
 
 use Moose;
+use Const::Fast;
+use Fcntl; # O_ constants
 use namespace::autoclean;
 
 extends qw( DesignCreate::Action );
 with 'DesignCreate::CmdRole::FindOligos';
+
+#TODO move this
+const my $DEFAULT_OLIGO_TARGET_REGIONS_DIR_NAME => 'oligo_target_regions';
 
 sub execute {
     my ( $self, $opts, $args ) = @_;
@@ -27,6 +32,20 @@ sub execute {
 
     return;
 }
+
+# if running command by itself we want to check the target regions dir exists
+# default is to delete and re-create folder
+override _build_oligo_target_regions_dir => sub {
+    my $self = shift;
+
+    my $target_regions_dir = $self->dir->subdir( $DEFAULT_OLIGO_TARGET_REGIONS_DIR_NAME );
+    unless ( $self->dir->contains( $target_regions_dir ) ) {
+        $self->log->logdie( "Can't find aos output dir: "
+                           . $target_regions_dir->stringify );
+    }
+
+    return $target_regions_dir->absolute;
+};
 
 __PACKAGE__->meta->make_immutable;
 
