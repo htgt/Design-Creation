d5d1401d52f9a8910bc7e1786f81d1b5d9dd6412
Sajith Perera <sajith.perera@gmail.com>
2013-02-25 13:28:46 +0000

    add test for run command

---
+++ t/lib/Test/DesignCreate/Action/Run.pm
@@ -0,0 +1,49 @@
+package Test::DesignCreate::Action::Run;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test::Most;
+use App::Cmd::Tester;
+use Path::Class qw( tempdir );
+use base qw( Test::Class Class::Data::Inheritable );
+
+use DesignCreate::Cmd;
+
+# Testing
+# DesignCreate::Action::Run ( through command line )
+
+BEGIN {
+    __PACKAGE__->mk_classdata( 'cmd_class' => 'DesignCreate::Cmd' );
+}
+
+sub valid_run_aos_cmd : Test(3) {
+    my $test = shift;
+
+    my $dir = File::Temp->newdir( TMPDIR => 1, CLEANUP => 1 );
+
+    my @argv_contents = (
+        'run',
+        '--dir', $dir->dirname,
+        '--target-start', 101176328,
+        '--target-end', 101176428,
+        '--chromosome', 11,
+        '--strand', 1,
+        '--target-gene', 'LBL-TEST',
+    );
+
+    note('############################################');
+    note('Following test may take a while to finish...');
+    note('############################################');
+    ok my $result = test_app($test->cmd_class => \@argv_contents), 'can run command';
+
+    is $result->stderr, '', 'no errors';
+    ok !$result->error, 'no command errors';
+
+    #change out of tmpdir so File::Temp can delete the tmp dir
+    chdir;
+}
+
+1;
+
+__END__
