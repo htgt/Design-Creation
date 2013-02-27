21c8869b33866d01a7030c9ee299a154f3bafbe8
Sajith Perera <sajith.perera@gmail.com>
2013-02-27 11:31:34 +0000

    add get test object method to test, so make it uniform with all the other tests

--- t/lib/Test/DesignCreate/CmdRole/OligoTargetRegions.pm
+++ t/lib/Test/DesignCreate/CmdRole/OligoTargetRegions.pm
@@ -46,67 +46,52 @@ sub valid_run_cmd : Test(3) {
     ok !$result->error, 'no command errors';
 }
 
-sub constructor : Test(startup => 3) {
+sub get_oligo_region_offset : Tests(4) {
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
-    my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
-
-    ok my $o = $test->test_class->new(
-        dir          => $dir,
-        target_start => 101176328,
-        target_end   => 101176428,
-        chr_name     => 11,
-        chr_strand   => 1,
-    ), 'we got a object';
-
-    is $o->dir->stringify, $dir->stringify, 'correct working dir';
-    is $o->design_method, 'deletion', 'correct design_method';
-
-    $test->{o} = $o;
-}
-
-sub get_oligo_region_offset : Tests(3) {
-    my $test = shift;
-
-    ok my $offset = $test->{o}->get_oligo_region_offset('G5'), 'can get_oligo_region_offset';
-    is $offset, $test->{o}->G5_region_offset, 'is expected offset value';
-    throws_ok { $test->{o}->get_oligo_region_offset('M3') }
+    ok my $offset = $o->get_oligo_region_offset('G5'), 'can get_oligo_region_offset';
+    is $offset, $o->G5_region_offset, 'is expected offset value';
+    throws_ok { $o->get_oligo_region_offset('M3') }
         qr/Attribute M3_region_offset does not exist/, 'throws error on unexpected oligo name';
 
 }
 
-sub get_oligo_region_length : Tests(3) {
+sub get_oligo_region_length : Tests(4) {
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
-    ok my $length = $test->{o}->get_oligo_region_length('G5'), 'can get_oligo_region_length';
-    is $length, $test->{o}->G5_region_length, 'have correct oligo region length value';
+    ok my $length = $o->get_oligo_region_length('G5'), 'can get_oligo_region_length';
+    is $length, $o->G5_region_length, 'have correct oligo region length value';
 
     throws_ok {
-        $test->{o}->get_oligo_region_length('M3')
+        $o->get_oligo_region_length('M3')
     } qr/Attribute M3_region_length does not exist/
         , 'throws error on unexpected oligo name';
 
 }
 
-sub write_sequence_file : Tests(5){
+sub write_sequence_file : Tests(6){
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
     lives_ok {
-        $test->{o}->write_sequence_file( 'A1', 'A1_test', 'ATCG' )
+        $o->write_sequence_file( 'A1', 'A1_test', 'ATCG' )
     } 'can write_sequence_file';
 
-    ok my $seq_file = $test->{o}->oligo_target_regions_dir->file( 'A1.fasta' )
+    ok my $seq_file = $o->oligo_target_regions_dir->file( 'A1.fasta' )
         ,'can create Path::Class::File object from oligo target regions directory';
-    ok $test->{o}->oligo_target_regions_dir->contains( $seq_file ), 'test seq file exists';
+    ok $o->oligo_target_regions_dir->contains( $seq_file ), 'test seq file exists';
 
-    ok my $seq_in = Bio::SeqIO->new( -file => $seq_file->stringify, -format => 'fasta' ), 'can load up seq file';
+    ok my $seq_in = Bio::SeqIO->new( -file => $seq_file->stringify, -format => 'fasta' )
+        , 'can load up seq file';
     is $seq_in->next_seq->seq, 'ATCG', 'file has correct sequence';
 }
 
-sub get_oligo_region_coordinates : Tests(8) {
+sub get_oligo_region_coordinates : Tests(9) {
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
-    my $o = $test->{o};
     ##
     ## Deletion / Insertion
     ##
@@ -125,9 +110,8 @@ sub get_oligo_region_coordinates : Tests(8) {
     is $u3_start, $u3_real_start, 'correct start value';
     is $u3_end, $u3_real_end, 'correct end value';
 
-    my $dir = File::Temp->newdir( TMPDIR => 1 );
     ok $o = $test->test_class->new(
-        dir               => dir( $dir->dirname ),
+        dir               => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute, 
         target_start      => 101176328,
         target_end        => 101176428,
         chr_name          => 11,
@@ -141,29 +125,44 @@ sub get_oligo_region_coordinates : Tests(8) {
 
 }
 
-sub build_oligo_target_regions : Test(5) {
+sub build_oligo_target_regions : Test(6) {
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
     lives_ok {
-        $test->{o}->build_oligo_target_regions
+        $o->build_oligo_target_regions
     } 'can build_oligo_target_regions';
 
     for my $oligo ( qw( G5 U5 D3 G3 ) ) {
-        my $oligo_file = $test->{o}->oligo_target_regions_dir->file( $oligo . '.fasta' );
-        ok $test->{o}->oligo_target_regions_dir->contains( $oligo_file ), "$oligo oligo file exists";
+        my $oligo_file = $o->oligo_target_regions_dir->file( $oligo . '.fasta' );
+        ok $o->oligo_target_regions_dir->contains( $oligo_file ), "$oligo oligo file exists";
     }
 }
 
-sub get_sequence : Test(2) {
+sub get_sequence : Test(3) {
     my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
 
-    my $seq = $test->{o}->get_sequence( 1,10 );
+    my $seq = $o->get_sequence( 1,10 );
     is $seq, 'NNNNNNNNNN', 'sequence is correct';
 
     throws_ok{
-        $test->{o}->get_sequence( 10, 1 );
+        $o->get_sequence( 10, 1 );
     } qr/Start must be less than end/
         , 'throws error if start after end';
+}
+
+sub _get_test_object {
+    my ( $test, $strand ) = @_;
+    $strand //= 1;
+
+    return $test->test_class->new(
+        dir          => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
+        target_start => 101176328,
+        target_end   => 101176428,
+        chr_name     => 11,
+        chr_strand   => $strand,
+    );
 }
 
 1;
