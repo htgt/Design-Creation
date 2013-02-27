24f7fdee5db3cff6e5670e0f36f9a5aa9bcd40d5
Sajith Perera <sajith.perera@gmail.com>
2013-02-27 08:27:20 +0000

    add documentation and command aliases to oligo region parameter attributes

--- lib/DesignCreate/Role/OligoRegionParameters.pm
+++ lib/DesignCreate/Role/OligoRegionParameters.pm
@@ -14,8 +14,6 @@ use Moose::Role;
 use DesignCreate::Types qw( PositiveInt NaturalNumber );
 use namespace::autoclean;
 
-# TODO add cmd_alias and documentation
-
 has target_start => (
     is            => 'ro',
     isa           => PositiveInt,
@@ -35,87 +33,111 @@ has target_end => (
 );
 
 has G5_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 1000,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 1000,
+    documentation => 'Length of G5 oligo candidate region',
+    cmd_flag      => 'g5-region-length'
 );
 
 has G5_region_offset => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 4000,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 4000,
+    documentation => 'Offset from target region of G5 oligo candidate region',
+    cmd_flag      => 'g5-region-offset'
 );
 
 has U5_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 200,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 200,
+    documentation => 'Length of U5 oligo candidate region',
+    cmd_flag      => 'u5-region-length'
 );
 
 has U5_region_offset => (
-    is      => 'ro',
-    isa     => NaturalNumber,
-    traits  => [ 'Getopt' ],
-    default => 0,
+    is            => 'ro',
+    isa           => NaturalNumber,
+    traits        => [ 'Getopt' ],
+    default       => 0,
+    documentation => 'Offset from target region of U5 oligo candidate region',
+    cmd_flag      => 'u5-region-offset'
 );
 
 has U3_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 200,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 200,
+    documentation => 'Length of U3 oligo candidate region',
+    cmd_flag      => 'u3-region-length'
 );
 
 has U3_region_offset => (
-    is      => 'ro',
-    isa     => NaturalNumber,
-    traits  => [ 'Getopt' ],
-    default => 0,
+    is            => 'ro',
+    isa           => NaturalNumber,
+    traits        => [ 'Getopt' ],
+    default       => 0,
+    documentation => 'Offset from target region of U3 oligo candidate region',
+    cmd_flag      => 'u3-region-offset'
 );
 
 has D5_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 200,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 200,
+    documentation => 'Length of D5 oligo candidate region',
+    cmd_flag      => 'd5-region-length'
 );
 
 has D5_region_offset => (
-    is      => 'ro',
-    isa     => NaturalNumber,
-    traits  => [ 'Getopt' ],
-    default => 0,
+    is            => 'ro',
+    isa           => NaturalNumber,
+    traits        => [ 'Getopt' ],
+    default       => 0,
+    documentation => 'Offset from target region of D5 oligo candidate region',
+    cmd_flag      => 'd5-region-offset'
 );
 
 has D3_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 200,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 200,
+    documentation => 'Length of D3 oligo candidate region',
+    cmd_flag      => 'd3-region-length'
 );
 
 has D3_region_offset => (
-    is      => 'ro',
-    isa     => NaturalNumber,
-    traits  => [ 'Getopt' ],
-    default => 0,
+    is            => 'ro',
+    isa           => NaturalNumber,
+    traits        => [ 'Getopt' ],
+    default       => 0,
+    documentation => 'Offset from target region of D3 oligo candidate region',
+    cmd_flag      => 'd3-region-offset'
 );
 
 has G3_region_length => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 1000,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 1000,
+    documentation => 'Length of G3 oligo candidate region',
+    cmd_flag      => 'g3-region-length'
 );
 
 has G3_region_offset => (
-    is      => 'ro',
-    isa     => PositiveInt,
-    traits  => [ 'Getopt' ],
-    default => 4000,
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    default       => 4000,
+    documentation => 'Offset from target region of G3 oligo candidate region',
+    cmd_flag      => 'g3-region-offset'
 );
 
 1;
