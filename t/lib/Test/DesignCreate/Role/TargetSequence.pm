5c7997053432134882dd9dc279faa3d8e7a3964a
Sajith Perera <sajith.perera@gmail.com>
2013-03-01 11:40:59 +0000

    add more tests to TargetSequence role

--- t/lib/Test/DesignCreate/Role/TargetSequence.pm
+++ t/lib/Test/DesignCreate/Role/TargetSequence.pm
@@ -29,14 +29,74 @@ sub get_sequence : Test(3) {
         , 'throws error if start after end';
 }
 
+sub chr_name : Test(3) {
+    my $test = shift;
+
+    my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
+
+    throws_ok{
+        $test->test_class->new( dir => $dir, chr_strand => 1, chr_name => 30 )
+    } qr/Invalid chromosome name, 30/, 'throws error with invalid chromosome name';
+
+    throws_ok{
+        $test->test_class->new( dir => $dir, chr_strand => 1, chr_name => 'Z' )
+    } qr/Invalid chromosome name, Z/, 'throws error with invalid chromosome name';
+
+    lives_ok{
+        $test->test_class->new( dir => $dir, chr_strand => -1, chr_name => 'y' )
+    } 'valid chromosome okay';
+}
+
+sub chr_strand : Test(3) {
+    my $test = shift;
+
+    my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
+
+    throws_ok{
+        $test->test_class->new( dir => $dir, chr_strand => 2, chr_name => '3' )
+    } qr/Invalid strand 2/, 'throws error with invalid chromosome name';
+
+    throws_ok{
+        $test->test_class->new( dir => $dir, chr_strand => -2, chr_name => 'X' )
+    } qr/Invalid strand -2/, 'throws error with invalid chromosome name';
+
+    lives_ok{
+        $test->test_class->new( dir => $dir, chr_strand => -1, chr_name => 'X' )
+    } 'valid strand okay';
+}
+
+sub species : Test(2) {
+    my $test = shift;
+
+    my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
+
+    throws_ok{
+        $test->test_class->new( dir => $dir, chr_strand => 1, chr_name => '3', species => 'Human' )
+    } qr/Invalid species Human/, 'throws error with invalid chromosome name';
+
+    lives_ok{
+        $test->test_class->new( dir => $dir, chr_strand => -1, chr_name => 'X', species => 'Mouse' )
+    } 'mouse species  okay';
+}
+
+sub ensembl_util : Test(5) {
+    my $test = shift;
+    ok my $o = $test->_get_test_object, 'can grab test object';
+
+    ok my $ensembl_util = $o->ensembl_util, 'can grab ensembl util';
+    isa_ok $ensembl_util, 'LIMS2::Util::EnsEMBL';
+
+    ok my $slice_adaptor = $o->ensembl_util->slice_adaptor, 'can grab slice adaptor';
+    isa_ok $slice_adaptor, 'Bio::EnsEMBL::DBSQL::SliceAdaptor';
+}
+
 sub _get_test_object {
-    my ( $test, $strand ) = @_;
-    $strand //= 1;
+    my  $test = shift;
 
     return $test->test_class->new(
-        dir         => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        chr_name    => 11,
-        chr_strand  => $strand,
+        dir        => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
+        chr_name   => 11,
+        chr_strand => 1,
     );
 }
 
