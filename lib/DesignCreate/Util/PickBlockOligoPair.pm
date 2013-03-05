b9f43f2b13f955040c19d63e33d94cdf57e0fd1d
Sajith Perera <sajith.perera@gmail.com>
2013-03-05 10:15:30 +0000

    add util module for finding valid pairs of oligos from a oligo block region, also tests for this module

---
+++ lib/DesignCreate/Util/PickBlockOligoPair.pm
@@ -0,0 +1,124 @@
+package DesignCreate::Util::PickBlockOligoPair;
+
+=head1 NAME
+
+DesignCreate::Util::PickBlockOligos - Pick the best block oligo pair
+
+=head1 DESCRIPTION
+
+Pick the best pair of oligos from a block region. 
+Pair must be a minimum distance apart.
+Best pair has smallest distance seperating them, that is bigger than the minimum distance.
+
+=cut
+
+use Moose;
+use DesignCreate::Exception;
+use DesignCreate::Types qw( PositiveInt Strand );
+use YAML::Any qw( LoadFile );
+use namespace::autoclean;
+
+with qw( MooseX::Log::Log4perl );
+
+has five_prime_oligo_file => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+);
+
+has three_prime_oligo_file => (
+    is       => 'ro',
+    isa      => 'Path::Class::File',
+    required => 1,
+);
+
+has min_gap => (
+    is       => 'ro',
+    isa      => PositiveInt,
+    required => 1,
+);
+
+has strand => (
+    is       => 'ro',
+    isa      => Strand,
+    required => 1,
+);
+
+has left_oligo_data => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+    traits     => [ 'Array' ],
+    handles    => {
+        sort_left_oligos => 'sort_in_place',
+        left_oligos      => 'elements',
+    },
+);
+
+sub _build_left_oligo_data {
+    my $self = shift;
+
+    my $oligo_file = $self->strand == 1 ? $self->five_prime_oligo_file : $self->three_prime_oligo_file;
+    return LoadFile( $oligo_file );
+}
+
+has right_oligo_data => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+    traits     => [ 'Array' ],
+    handles    => {
+        sort_right_oligos => 'sort_in_place',
+        right_oligos      => 'elements',
+    },
+);
+
+sub _build_right_oligo_data {
+    my $self = shift;
+
+    my $oligo_file = $self->strand == 1 ? $self->three_prime_oligo_file : $self->five_prime_oligo_file;
+    return LoadFile( $oligo_file );
+}
+
+# left oligos ranked from closest to 3' end to furthest
+# right oligos ranked from cloesest to 5' end to furthest
+# subroutine will list pairs from closest together to furthest apart
+sub get_oligo_pairs {
+    my $self = shift;
+    my @oligo_pairs;
+
+    # rank right oligos from closest to 5' to furthest
+    $self->sort_right_oligos( sub{ $_[0]->{offset} <=> $_[1]->{offset} } );
+    # rank left oligos from closest to 3' to furthest
+    $self->sort_left_oligos( sub{ $_[1]->{offset} <=> $_[0]->{offset} } );
+
+    for my $left_oligo ( $self->left_oligos ) {
+        for my $right_oligo ( $self->right_oligos ) {
+            DesignCreate::Exception->throw( 'Invalid input '
+                    . $left_oligo->{id} . ' and ' . $right_oligo->{id}
+                    . ' overlap, error with input' )
+                if $left_oligo->{oligo_end} > $right_oligo->{oligo_start};
+
+            my $oligo_gap = ( $right_oligo->{oligo_start} - $left_oligo->{oligo_end} ) - 1;
+            if ( $oligo_gap < $self->min_gap ) {
+                $self->log->debug( $left_oligo->{id} . ' and '
+                        . $right_oligo->{id} . " have a gap of $oligo_gap,"
+                        . " minimum gap is " . $self->min_gap );
+                next;
+            }
+
+            push @oligo_pairs, {
+                $left_oligo->{oligo}  => $left_oligo->{id},
+                $right_oligo->{oligo} => $right_oligo->{id}
+            };
+        }
+    }
+    
+    return \@oligo_pairs;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
---
+++ t/lib/Test/DesignCreate/Util/PickBlockOligoPair.pm
@@ -0,0 +1,125 @@
+package Test::DesignCreate::Util::PickBlockOligoPair;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test::Most;
+use FindBin;
+use Path::Class qw( dir );
+use base qw( Test::Class Class::Data::Inheritable );
+
+use DesignCreate::Util::PickBlockOligoPair;
+
+# Testing
+# DesignCreate::Util::PickBlockOligoPair
+
+BEGIN {
+    __PACKAGE__->mk_classdata( 'class' => 'DesignCreate::Util::PickBlockOligoPair' );
+}
+
+sub constructor : Test( startup => 2) {
+    my $test = shift;
+    my $o;
+
+    lives_ok {
+        $o = $test->get_test_object()
+    } 'can create test object';
+
+    isa_ok $o, $test->class;
+}
+
+sub right_oligo_data : Test(9) {
+    my $test = shift;
+    ok my $o = $test->get_test_object, 'can get test object';
+
+    ok my @right_oligos = $o->right_oligos, 'can get right oligos';
+    is $right_oligos[0]->{oligo}, 'U3', 'correct oligo type for +ve stranded design';
+
+    ok my @sorted_oligos = sort( { $a->{offset} <=> $b->{offset} } @right_oligos ), 'sort oligo data';
+    lives_ok {
+        $o->get_oligo_pairs
+    } 'can call get_oligo_pairs';
+
+    is_deeply $o->right_oligo_data, \@sorted_oligos, 'right oligos sorted correctly';
+
+    ok $o = $test->get_test_object( { strand => -1 } ), 'can get another test object';
+    ok @right_oligos = $o->right_oligos, 'can get right oligos again';
+    is $right_oligos[0]->{oligo}, 'U5', 'correct oligo type for -ve stranded design';
+}
+
+sub left_oligo_data : Test(9) {
+    my $test = shift;
+    ok my $o = $test->get_test_object, 'can get test object';
+
+    ok my @left_oligos = $o->left_oligos, 'can get left oligos';
+    is $left_oligos[0]->{oligo}, 'U5', 'correct oligo type for +ve stranded design';
+
+    ok my @sorted_oligos = sort( { $b->{offset} <=> $a->{offset} } @left_oligos ), 'sort oligo data';
+    lives_ok {
+        $o->get_oligo_pairs
+    } 'can call get_oligo_pairs';
+
+    is_deeply $o->left_oligo_data, \@sorted_oligos, 'left oligos sorted correctly';
+
+    ok $o = $test->get_test_object( { strand => -1 } ), 'can get another test object';
+    ok @left_oligos = $o->left_oligos, 'can get left oligos again';
+    is $left_oligos[0]->{oligo}, 'U3', 'correct oligo type for -ve stranded design';
+}
+
+sub get_oligo_pairs : Test(11) {
+    my $test = shift;
+    ok my $o = $test->get_test_object, 'can get test object';
+
+    #default min_gap 15 makes the U3-10, U5-13 pair invalid, their gap is 14
+    ok my $oligo_pairs = $o->get_oligo_pairs,  'can call get_oligo_pairs';
+
+    my $best_pair = shift @{ $oligo_pairs };
+    is_deeply $best_pair, { U5 => 'U5-13', U3 => 'U3-9' }, 'have correct best pair';
+
+    #min_gap 10 makes teh U3-10, U5-13 pair valid, their gap is 14
+    ok $o = $test->get_test_object( { min_gap => 10 } ), 'can get another test object';
+    ok $oligo_pairs = $o->get_oligo_pairs,  'can call get_oligo_pairs';
+
+    $best_pair = shift @{ $oligo_pairs };
+    is_deeply $best_pair, { U5 => 'U5-13', U3 => 'U3-10' }, 'have correct best pair';
+
+    ok my $d_o = $test->get_test_object( { five_file => 'D5.yaml', three_file => 'D3.yaml' } ),
+        'can get test object for D oligos';
+
+    ok my $d_oligo_pairs = $d_o->get_oligo_pairs,  'can call get_oligo_pairs';
+    is_deeply $d_oligo_pairs, [], 'no valid pairs';
+
+    ok my $f_o = $test->get_test_object( { strand => -1 } ), 'can get test object with wrong strand';
+    throws_ok{
+        $f_o->get_oligo_pairs
+    } qr/Invalid input/, 'throws error when 5 and 3 oligos wrong way around for given strand';
+}
+
+sub get_test_object {
+    my ( $test, $params ) = @_;
+    my $strand     = $params->{strand} || 1;
+    my $min_gap    = $params->{min_gap} || 15;
+    my $five_file  = $params->{five_file} || 'U5.yaml';
+    my $three_file = $params->{three_file} ||'U3.yaml';
+
+    my $o = $test->class->new(
+        five_prime_oligo_file  => _get_test_data_file( $five_file ),
+        three_prime_oligo_file => _get_test_data_file( $three_file ),
+        min_gap                => $min_gap,
+        strand                 => $strand,
+    );
+
+    return $o;
+}
+
+sub _get_test_data_file {
+    my ( $filename ) = @_;
+    my $data_dir = dir($FindBin::Bin)->subdir('test_data/pick_block_oligos_data/');
+    my $file = $data_dir->file($filename);
+
+    return $file;
+}
+
+1;
+
+__END__
