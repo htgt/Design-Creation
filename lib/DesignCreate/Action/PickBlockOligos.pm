84d9921854bb254e3873c0191c78b67931c886cb
Sajith Perera <sajith.perera@gmail.com>
2013-03-05 11:45:54 +0000

    add pick-block-oligos command

---
+++ lib/DesignCreate/Action/PickBlockOligos.pm
@@ -0,0 +1,57 @@
+package DesignCreate::Action::PickBlockOligos;
+
+=head1 NAME
+
+DesignCreate::Action::PickBlockOligos - Pick the best U and D block oligo pairs
+
+=head1 DESCRIPTION
+
+Pick the best pair of block oligos ( U5 & U3, D5 and D3).
+Must have a minumum gap between the oligo pairs.
+We prefer closer pairs of oligos.
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use Const::Fast;
+use Try::Tiny;
+use namespace::autoclean;
+
+extends qw( DesignCreate::Action );
+with 'DesignCreate::CmdRole::PickBlockOligos';
+
+sub execute {
+    my ( $self, $opts, $args ) = @_;
+
+    try{
+        $self->pick_block_oligos;
+    }
+    catch{
+        $self->log->error( "Error picking block oligos:\n" . $_ );
+    };
+
+    return;
+}
+
+# if running command by itself we want to check the validate oligo dir exists
+# default is to delete and re-create folder
+override _build_validated_oligo_dir => sub {
+    my $self = shift;
+
+    my $validated_oligo_dir = $self->dir->subdir( $self->validated_oligo_dir_name );
+    unless ( $self->dir->contains( $validated_oligo_dir ) ) {
+        $self->log->logdie( "Can't find validated oligo file dir: "
+                           . $validated_oligo_dir->stringify );
+    }
+
+    return $validated_oligo_dir->absolute;
+};
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
---
+++ lib/DesignCreate/CmdRole/PickBlockOligos.pm
@@ -0,0 +1,81 @@
+package DesignCreate::CmdRole::PickBlockOligos;
+
+=head1 NAME
+
+DesignCreate::CmdRole::PickBlockOligos - Pick the best U and D block oligo pairs
+
+=head1 DESCRIPTION
+
+Pick the best pair of block oligos ( U5 & U3, D5 & D3 ).
+Pair must be a minimum distance apart.
+Best pair has smallest distance seperating them, that is bigger than the minimum distance.
+
+=cut
+
+use Moose::Role;
+use DesignCreate::Exception;
+use DesignCreate::Types qw( PositiveInt );
+use YAML::Any qw( DumpFile );
+use DesignCreate::Util::PickBlockOligoPair;
+use namespace::autoclean;
+
+with qw(
+DesignCreate::Role::TargetSequence
+);
+
+has min_U_oligo_gap => (
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    documentation => 'Minimum gap between U oligos',
+    required      => 1,
+    cmd_flag      => 'min-U-oligo-gap',
+);
+
+has min_D_oligo_gap => (
+    is            => 'ro',
+    isa           => PositiveInt,
+    traits        => [ 'Getopt' ],
+    documentation => 'Minimum gap between D oligos',
+    required      => 1,
+    cmd_flag      => 'min-D-oligo-gap',
+);
+
+sub pick_block_oligos {
+    my ( $self, $opts, $args ) = @_;
+
+    $self->pick_block_oligo_pair( $_ ) for qw( U D );
+
+    return;
+}
+
+sub pick_block_oligo_pair {
+    my ( $self, $oligo_type ) = @_;
+
+    my $min_gap_attribute = 'min_' . $oligo_type . '_oligo_gap';
+    DesignCreate::Exception->throw( "Attribute $min_gap_attribute does not exist" )
+        unless $self->meta->has_attribute( $min_gap_attribute );
+
+    my $five_prime_oligo_file = $self->get_file( $oligo_type . '5.yaml' , $self->validated_oligo_dir );
+    my $three_prime_oligo_file = $self->get_file( $oligo_type . '3.yaml', $self->validated_oligo_dir );
+
+    my $oligo_picker = DesignCreate::Util::PickBlockOligoPair->new(
+        five_prime_oligo_file  => $five_prime_oligo_file,
+        three_prime_oligo_file => $three_prime_oligo_file,
+        strand                 => $self->chr_strand,
+        min_gap                => $self->$min_gap_attribute,
+    );
+
+    my $oligo_pairs = $oligo_picker->get_oligo_pairs;
+    DesignCreate::Exception->throw( "No valid oligo pairs for $oligo_type oligo region" )
+        unless @{ $oligo_pairs };
+
+    my $oligo_pair_file =  $self->validated_oligo_dir->file( $oligo_type . '_oligo_pairs.yaml');
+    DumpFile( $oligo_pair_file, $oligo_pairs );
+
+    return;
+}
+
+1;
+
+__END__
