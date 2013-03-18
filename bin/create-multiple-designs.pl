dfd6022c9b7a8d36da7207e5bc5130c5f98f1ac0
Sajith Perera <sajith.perera@gmail.com>
2013-03-18 10:01:37 +0000

    add pod

--- bin/create-multiple-designs.pl
+++ bin/create-multiple-designs.pl
@@ -1,9 +1,5 @@
 #!/usr/bin/env perl
 
-#
-# Outline of a script that can create multiple designs from information specified in a csv file
-#
-
 use strict;
 use warnings FATAL => 'all';
 
@@ -12,10 +8,14 @@ use Text::CSV;
 use Try::Tiny;
 use Getopt::Long;
 use List::MoreUtils qw( any );
+use Pod::Usage;
+
 use Smart::Comments;
 
 my ( $file, $persist, $base_work_dir );
 GetOptions(
+    'help'        => sub { pod2usage( -verbose => 1 ) },
+    'man'         => sub { pod2usage( -verbose => 2 ) },
     'file=s'      => \$file,
     'persist'     => \$persist,
     'dir=s'       => \$base_work_dir,
@@ -23,7 +23,7 @@ GetOptions(
     'debug'       => \my $debug,
     'gene=s'      => \my @genes,
     'param=s'     => \my %extra_params,
-);
+) or pod2usage(2);
 
 die( 'Specify base work dir' ) unless $base_work_dir;
 die( 'Specify file with design info' ) unless $file;
@@ -73,3 +73,42 @@ sub get_params {
 
     return ( \@params, $dir );
 }
+
+__END__
+
+=head1 NAME
+
+create-multiple-designs.pl - Create multiple designs
+
+=head1 SYNOPSIS
+
+  create-multiple-designs.pl [options]
+
+      --help            Display a brief help message
+      --man             Display the manual page
+      --debug           Print debug messages
+      --file            File with design details.
+      --persist         Persist newly created designs to LIMS2
+      --dir             Directory where design-create output goes
+      --conditional     Specify conditional design, default deletion
+      --gene            Only create this gene(s), picked from input file
+      --param           Specify additional param(s) not in file
+
+=head1 DESCRIPTION
+
+Takes design information for multiple designs from file and tries to create these designs.
+
+The file will be a csv file, each row represents a design, each column a parameter given to the
+design create command. The column headers represent the parameter name.
+
+=head1 AUTHOR
+
+Sajith Perera
+
+=head1 BUGS
+
+None reported... yet.
+
+=head1 TODO
+
+=cut
