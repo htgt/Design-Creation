355bfd8c7e3f3c760c403a85cda3218d7dedc98c
Sajith Perera <sajith.perera@gmail.com>
2013-05-22 08:05:09 +0100

    add target exon to log output for del-exon command

--- lib/DesignCreate/Action/DelExonDesign.pm
+++ lib/DesignCreate/Action/DelExonDesign.pm
@@ -63,6 +63,7 @@ for my $attribute ( @ATTRIBUTES_NO_CMD_OPTION ) {
 sub execute {
     my ( $self, $opts, $args ) = @_;
     Log::Log4perl::NDC->push( @{ $self->target_genes }[0] );
+    Log::Log4perl::NDC->push( $self->target_exon );
 
     $self->log->info( 'Starting new design create run: ' . join(',', @{ $self->target_genes } ) );
     $self->log->debug( 'Design run args: ' . pp($opts) );
