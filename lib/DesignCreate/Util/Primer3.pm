cfec2ccec390ebc04baabad79a0fe40376cfeb23
Sajith Perera <sajith.perera@gmail.com>
2013-07-19 10:59:04 +0100

    primer3 util module now sets up primer3 with global arguments and can be called multiple time with specific sequence and target criteria

--- lib/DesignCreate/Util/Primer3.pm
+++ lib/DesignCreate/Util/Primer3.pm
@@ -4,13 +4,13 @@ use Moose;
 use namespace::autoclean;
 use Bio::Tools::Primer3Redux;
 use Bio::Tools::Run::Primer3Redux;
-use MooseX::Types::Path::Class::MoreCoercions qw/AbsFile/;
 use DesignCreate::Types qw( PositiveInt );
 use Const::Fast;
+use Scalar::Util qw( blessed reftype );
 
 with qw( MooseX::Log::Log4perl MooseX::SimpleConfig );
 
-const my @PRIMER3_ARGUMENTS => (
+const my @PRIMER3_GLOBAL_ARGUMENTS => (
     'primer_num_return',
     'primer_min_size',
     'primer_max_size',
@@ -26,39 +26,11 @@ const my @PRIMER3_ARGUMENTS => (
 );
 
 #TODO change path to primer3 sp12 Wed 17 Jul 2013 10:13:33 BST
-has primer3_path => ( 
+has primer3_path => (
     is       => 'ro',
     isa      => 'Str',
     required => 1,
     default  => '/nfs/users/nfs_s/sp12/workspace/primer3-2.3.5/src/primer3_core',
-);
-
-# modify to work relative to WORK_DIR
-has outfile => (
-    is       => 'ro',
-    isa      => AbsFile,
-    coerce   => 1,
-    required => 1,
-);
-
-has seq => (
-    is       => 'ro',
-    isa      => 'Bio::SeqI',
-    required => 1,
-);
-
-# Number of bases target region starts from the start of the sequence
-has target_region_start_offset => (
-    is       => 'ro',
-    isa      => PositiveInt,
-    required => 1,
-);
-
-# Number of bases target region ends from the end of the sequence
-has target_region_end_offset => (
-    is       => 'ro',
-    isa      => PositiveInt,
-    required => 1,
 );
 
 has [
@@ -103,17 +75,17 @@ has primer_min_three_prime_distance => (
     required => 1,
 ) ;
 
-has primer3_arguments => (
+has primer3_global_arguments => (
     is         => 'ro',
     isa        => 'HashRef',
     lazy_build => 1
 );
 
-sub _build_primer3_arguments {
+sub _build_primer3_global_arguments {
     my $self = shift;
     my %primer3_arguments;
 
-    foreach (@PRIMER3_ARGUMENTS) {
+    foreach (@PRIMER3_GLOBAL_ARGUMENTS) {
         $primer3_arguments{ uc($_) } = $self->$_ if $self->$_;
     }
 
@@ -124,28 +96,36 @@ sub _build_primer3_arguments {
 #Run Primer3 with passed in arguments and targetting information from
 #the PrimerTarget object
 #
-sub run_primer3 {
-    my $self = shift;
 
-    my $primer3 = Bio::Tools::Run::Primer3Redux->new(
-        -seq     => $self->seq,
-        -outfile => $self->outfile,
-        -path    => $self->primer3_path
-    );
-    
-    unless ($primer3->executable) {
-        die( "primer3 can not be found");
+#TODO add logging sp12 Thu 18 Jul 2013 10:49:36 BST
+sub run_primer3 {
+    my ( $self, $outfile, $seq, $target ) = @_;
+
+    #TODO throw exceptions sp12 Thu 18 Jul 2013 10:49:20 BST
+    if ( ! blessed( $outfile ) || ! $outfile->isa( 'Path::Class::File' ) ) {
+        die( 'Outfile variable  must be Path::Class::File object' );
+    }
+    if ( ! blessed( $seq ) || ! $seq->isa( 'Bio::SeqI' ) ) {
+        die( 'Sequence variable must be Bio::SeqI object' );
+    }
+    if ( $target && reftype $target ne 'HASH' ) {
+        die( 'Target variable must be a hashref or undef' );
     }
 
-    $primer3->set_parameters( %{ $self->primer3_arguments } );
+    my $primer3 = Bio::Tools::Run::Primer3Redux->new(
+        -outfile => $outfile->stringify,
+        -path    => $self->primer3_path
+    );
+    die( "primer3 can not be found" ) unless $primer3->executable;
 
-    #CUSTOM
-    my $length = $self->seq->length;
-    $primer3->set_parameters( SEQUENCE_TARGET => $self->target_region_start_offset . ','
-            . ( $length - $self->target_region_end_offset - $self->target_region_start_offset ) );
+    $primer3->set_parameters( %{ $self->primer3_global_arguments } );
 
+    # Setup specific target which primers must flank
+    if ( $target && exists $target->{SEQUENCE_TARGET} ) {
+        $primer3->set_parameters( SEQUENCE_TARGET => $target->{SEQUENCE_TARGET} );
+    }
 
-    my $results = $primer3->pick_pcr_primers( $self->seq );
+    my $results = $primer3->pick_pcr_primers( $seq );
     my $result = $results->next_result;
     unless ( $result ) {
         die( "No results returned from primer3" );
