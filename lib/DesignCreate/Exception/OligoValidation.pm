f98efbdf05128992d1ae8cd8d31619369345b1f5
Sajith Perera <sajith.perera@gmail.com>
2013-10-09 08:33:41 +0100

    add 2 new expection classes, one for failed primer3 run, another for failed oligo validation

---
+++ lib/DesignCreate/Exception/OligoValidation.pm
@@ -0,0 +1,51 @@
+package DesignCreate::Exception::OligoValidation;
+
+use Moose;
+use namespace::autoclean;
+
+extends qw( DesignCreate::Exception );
+
+has '+message' => (
+    default => 'Failed to find any valid oligos for one or more oligo types'
+);
+
+has oligo_types => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+# $reason{'region'}{'left'}
+has invalid_reasons => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+override as_string => sub {
+    my $self = shift;
+
+    my $str = 'Failed to find any valid oligos of types: ' . join(',', @{ $self->oligo_types } );
+
+    if ( $self->show_stack_trace ) {
+        $str .= "\n\n" . $self->stack_trace->as_string;
+    }
+
+    return $str;
+};
+
+around as_hash => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $hash = $self->$orig;
+
+    $hash->{oligo_Types} = $self->oligo_types;
+    $hash->{reasons} = $self->invalid_reasons;
+
+    return $hash;
+};
+
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
+
+1;
---
+++ lib/DesignCreate/Exception/Primer3FailedFindOligos.pm
@@ -0,0 +1,51 @@
+package DesignCreate::Exception::Primer3FailedFindOligos;
+
+use Moose;
+use namespace::autoclean;
+
+extends qw( DesignCreate::Exception );
+
+has '+message' => (
+    default => 'Primer3 failed to find oligos'
+);
+
+has regions => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+# $reason{'region'}{'left'}
+has primer_fail_reasons => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+override as_string => sub {
+    my $self = shift;
+
+    my $str = 'Primer3 failed to find oligos for following regions: ' . join(',', @{ $self->regions } );
+
+    if ( $self->show_stack_trace ) {
+        $str .= "\n\n" . $self->stack_trace->as_string;
+    }
+
+    return $str;
+};
+
+around as_hash => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $hash = $self->$orig;
+
+    $hash->{regions} = $self->regions;
+    $hash->{reasons} = $self->primer_fail_reasons;
+
+    return $hash;
+};
+
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
+
+1;
