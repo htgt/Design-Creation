780280b6e83e5c0b35aeb9352c88cc0f797fc048
Sajith Perera <sajith.perera@gmail.com>
2014-01-29 14:30:35 +0000

    allow multi exon targeting with gibson designs

--- lib/DesignCreate/CmdRole/OligoPairRegionsGibson.pm
+++ lib/DesignCreate/CmdRole/OligoPairRegionsGibson.pm
@@ -31,7 +31,8 @@ design_method
 species
 assembly
 target_genes
-target_exon
+five_prime_exon
+three_prime_exon
 target_start
 target_end
 chr_name
@@ -50,13 +51,22 @@ region_length_ER
 region_length_3F
 );
 
-has target_exon => (
+has five_prime_exon => (
     is            => 'ro',
     isa           => 'Str',
     traits        => [ 'Getopt' ],
-    documentation => 'EnsEMBL exon id we are targeting',
+    documentation => 'EnsEMBL exon id for five prime exon, if targeting only one exon use this',
     required      => 1,
-    cmd_flag      => 'target-exon'
+    cmd_flag      => 'five-prime-exon',
+    cmd_aliases   => 'target-exon', # keep old name as legacy
+);
+
+has three_prime_exon => (
+    is            => 'ro',
+    isa           => 'Str',
+    traits        => [ 'Getopt' ],
+    documentation => 'EnsEMBL exon id for last exon, only used if specifying range of exons to target',
+    cmd_flag      => 'three-prime-exon',
 );
 
 has design_method => (
@@ -185,77 +195,29 @@ sub _build_region_length_EF {
     return int( shift->region_length_5R_EF / 2 );
 }
 
-has exon => (
-    is         => 'ro',
-    isa        => 'Bio::EnsEMBL::Exon',
-    traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
-);
-
-sub _build_exon {
-    my $self = shift;
-    my $exon;
-
-    try{
-        $exon = $self->exon_adaptor->fetch_by_stable_id( $self->target_exon );
-    };
-
-    unless ( $exon ) {
-        DesignCreate::Exception->throw(
-            'Unable to retrieve exon: ' . $self->target_exon );
-    }
-
-    # check exon is on the chromosome coordinate system
-    if ( $exon->coord_system_name ne 'chromosome' ) {
-        $exon = $exon->transform( 'chromosome' );
-    }
-
-    return $exon;
-}
-
 has target_start => (
-    is         => 'ro',
+    is         => 'rw',
     isa        => PositiveInt,
     traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
  );
 
-sub _build_target_start {
-    return shift->exon->seq_region_start;
-}
-
 has target_end => (
-    is         => 'ro',
+    is         => 'rw',
     isa        => PositiveInt,
     traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
 );
-
-sub _build_target_end {
-    return shift->exon->seq_region_end;
-}
 
 has chr_name => (
-    is         => 'ro',
+    is         => 'rw',
     isa        => Chromosome,
     traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
 );
-
-sub _build_chr_name {
-    return shift->exon->seq_region_name;
-}
 
 has chr_strand => (
-    is         => 'ro',
+    is         => 'rw',
     isa        => Strand,
     traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
 );
-
-sub _build_chr_strand {
-    return shift->exon->strand;
-}
 
 has exon_region_start => (
     is         => 'ro',
@@ -396,6 +358,48 @@ sub _build_three_prime_region_end {
 
 =head2 get_oligo_pair_region_coordinates
 
+Calculate following values:
+target_start
+target_end
+chr_strand
+chr_name
+
+=cut
+sub BUILD {
+    my $self = shift;
+
+    my $five_prime_exon = $self->build_exon( $self->five_prime_exon );
+    $self->chr_name( $five_prime_exon->seq_region_name ) unless $self->chr_name;
+    $self->chr_strand( $five_prime_exon->strand ) unless $self->chr_strand;
+
+    my $three_prime_exon = $self->build_exon( $self->three_prime_exon )
+        if $self->three_prime_exon;
+
+    # if there is no three prime exon then just specify target start and end
+    # as the start and end of the five prime exon
+    unless ( $three_prime_exon ) {
+        $self->target_start( $five_prime_exon->seq_region_start );
+        $self->target_end( $five_prime_exon->seq_region_end );
+        return;
+    }
+
+    $self->validate_exon_targets( $five_prime_exon, $three_prime_exon );
+
+    if ( $self->chr_strand == 1 ) {
+        $self->target_start( $five_prime_exon->seq_region_start );
+        $self->target_end( $three_prime_exon->seq_region_end );
+    }
+    else {
+        $self->target_start( $three_prime_exon->seq_region_start );
+        $self->target_end( $five_prime_exon->seq_region_end );
+    }
+
+    return;
+}
+
+
+=head2 get_oligo_pair_region_coordinates
+
 Get coordinates for the three oligo pair regions:
 exon
 five_prime
@@ -437,9 +441,76 @@ sub check_oligo_region_sizes {
         my $length_attr =  'region_length_' . $oligo_type;
         my $length = $self->$length_attr;
 
-        # currently 22 is the smaller oligo we allow from primer
+        # currently 22 is the smallest oligo we allow from primer
         DesignCreate::Exception->throw( "$oligo_type region too small: $length" )
             if $length < 22;
+    }
+
+    return;
+}
+
+=head2 build_exon
+
+Grab a Bio::Ensembl::Exon object given a exon stable id.
+Make sure exon object is in teh chromosome coordinate system.
+
+=cut
+sub build_exon {
+    my ( $self, $exon_id ) = @_;
+    my $exon;
+
+    try{
+        $exon = $self->exon_adaptor->fetch_by_stable_id( $exon_id );
+    };
+
+    unless ( $exon ) {
+        DesignCreate::Exception->throw(
+            'Unable to retrieve exon: ' . $exon_id );
+    }
+
+    # check exon is on the chromosome coordinate system
+    if ( $exon->coord_system_name ne 'chromosome' ) {
+        $exon = $exon->transform( 'chromosome' );
+    }
+
+    return $exon;
+}
+
+=head2 validate_exon_targets
+
+Check exons are both on the same gene.
+Check the five prime exon is before the three prime exon.
+
+=cut
+sub validate_exon_targets {
+    my ( $self, $five_prime_exon, $three_prime_exon ) = @_;
+
+    my $five_prime_exon_gene  = $self->gene_adaptor->fetch_by_exon_stable_id( $five_prime_exon->stable_id );
+    my $three_prime_exon_gene = $self->gene_adaptor->fetch_by_exon_stable_id( $three_prime_exon->stable_id );
+
+    DesignCreate::Exception->throw(
+        'Exon mismatch,'
+        . ' five prime exon '      . $five_prime_exon->stable_id
+        . ' is linked to gene '    . $five_prime_exon_gene->stable_id
+        . ' BUT three prime exon ' . $three_prime_exon->stable_id
+        . ' is linked to gene  '   . $three_prime_exon_gene->stable_id
+    ) if $five_prime_exon_gene->stable_id ne $three_prime_exon_gene->stable_id;
+
+    if ( $self->chr_strand == 1 ) {
+        DesignCreate::Exception->throw(
+            'On +ve strand, five prime exon ' . $five_prime_exon->stable_id
+            . ' start ' . $five_prime_exon->seq_region_start
+            . ' is after the three prime exon ' . $three_prime_exon->stable_id
+            . ' start ' . $three_prime_exon->seq_region_start
+        ) if $five_prime_exon->seq_region_start > $three_prime_exon->seq_region_start;
+    }
+    else {
+        DesignCreate::Exception->throw(
+            'On -ve strand, five prime exon ' . $five_prime_exon->stable_id
+            . ' start ' . $five_prime_exon->seq_region_start
+            . ' is before the three prime exon ' . $three_prime_exon->stable_id
+            . ' start ' . $three_prime_exon->seq_region_start
+        ) if $five_prime_exon->seq_region_start < $three_prime_exon->seq_region_start;
     }
 
     return;
