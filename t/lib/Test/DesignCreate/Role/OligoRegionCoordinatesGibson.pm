1b3b0a1fd4d46f4327ea011da2d823f625de5c4a
Sajith Perera <sajith.perera@gmail.com>
2014-02-06 08:17:31 +0000

    Update tests for Role::OligoPairRegionsGibson after refactor

--- t/lib/Test/DesignCreate/Role/OligoRegionCoordinatesGibson.pm
+++ t/lib/Test/DesignCreate/Role/OligoRegionCoordinatesGibson.pm
@@ -5,6 +5,7 @@ use warnings FATAL => 'all';
 
 use Test::Most;
 use Path::Class qw( tempdir dir );
+use File::Copy::Recursive qw( dircopy );
 use Bio::SeqIO;
 use base qw( Test::DesignCreate::Class Class::Data::Inheritable );
 
@@ -15,111 +16,15 @@ BEGIN {
     __PACKAGE__->mk_classdata( 'test_role' => 'DesignCreate::Role::OligoRegionCoordinatesGibson' );
 }
 
-sub calculate_target_region_coordinates : Tests(17) {
-    my $test = shift;
-    my $metaclass = $test->get_test_object_metaclass( [ 'DesignCreate::CmdRole::OligoPairRegionsGibson' ] );
-
-    note( 'Single exon targets' );
-    ok my $o = $test->_get_test_object, 'can grab test object';
-    ok my $exon = $o->build_exon( $o->five_prime_exon ), 'can grab exon';
-
-    lives_ok {
-        $o->calculate_target_region_coordinates
-    } 'can call calculate_target_region_coordinates';
-    is $o->target_start, $exon->seq_region_start, 'target start is correct';
-    is $o->target_end, $exon->seq_region_end, 'target end is correct';
-
-    note( 'Multi exon targets, -ve strand' );
-    ok my $cbx1_obj = $metaclass->new_object(
-        dir              => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species          => 'Human',
-        five_prime_exon  => 'ENSE00001515177',
-        three_prime_exon => 'ENSE00002771605',
-        target_genes     => [ 'cbx1' ],
-    ), 'can grab new object with multi exon targets on -ve strand';
-    ok my $exon_5p = $o->build_exon( $cbx1_obj->five_prime_exon ), 'can grab 5 prime exon';
-    ok my $exon_3p = $o->build_exon( $cbx1_obj->three_prime_exon ), 'can grab 3 prime exon';
-    lives_ok {
-        $cbx1_obj->calculate_target_region_coordinates
-    } 'can call calculate_target_region_coordinates';
-    is $cbx1_obj->target_start, $exon_3p->seq_region_start, 'target start is correct';
-    is $cbx1_obj->target_end, $exon_5p->seq_region_end, 'target end is correct';
-
-    note( 'Multi exon targets, +ve strand' );
-    ok my $brac2_obj = $metaclass->new_object(
-        dir              => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species          => 'Human',
-        five_prime_exon  => 'ENSE00001484009',
-        three_prime_exon => 'ENSE00003659301 ',
-        target_genes     => [ 'brac2' ],
-    ), 'can grab new object with multi exon targets';
-    ok $exon_5p = $o->build_exon( $brac2_obj->five_prime_exon ), 'can grab 5 prime exon';
-    ok $exon_3p = $o->build_exon( $brac2_obj->three_prime_exon ), 'can grab 3 prime exon';
-    lives_ok {
-        $brac2_obj->calculate_target_region_coordinates
-    } 'can call calculate_target_region_coordinates';
-    is $brac2_obj->target_start, $exon_5p->seq_region_start, 'target start is correct';
-    is $brac2_obj->target_end, $exon_3p->seq_region_end, 'target end is correct';
-}
-
-sub validate_exon_targets : Tests(13) {
-    my $test = shift;
-    my $metaclass = $test->get_test_object_metaclass( [ 'DesignCreate::CmdRole::OligoPairRegionsGibson' ] );
-    ok my $o = $test->_get_test_object, 'can grab test object';
-
-    ok my $h2afx_exon = $o->build_exon( 'ENSE00002184393' ), 'can grab exon';
-    ok my $cbx1_exon_5p = $o->build_exon( 'ENSE00001515177' ), 'can grab exon';
-    ok my $cbx1_exon_3p = $o->build_exon( 'ENSE00002771605' ), 'can grab exon';
-
-    throws_ok{
-        $o->validate_exon_targets( $h2afx_exon, $cbx1_exon_5p )
-    } qr/Exon mismatch/, 'throws error if exons belong to different genes';
-
-    # -ve strand exons
-    # validate_exon_targets is called in BUILD method
-    ok my $cbx1_obj = $metaclass->new_object(
-        dir              => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species          => 'Human',
-        five_prime_exon  => 'ENSE00001515177',
-        three_prime_exon => 'ENSE00002771605',
-        target_genes     => [ 'cbx1' ],
-    ), 'can grab new object with multi exon targets on -ve strand';
-    ok $cbx1_obj->chr_strand(-1), 'set strand as -1';
-
-    throws_ok{
-        $cbx1_obj->validate_exon_targets( $cbx1_exon_3p, $cbx1_exon_5p )
-    } qr/On -ve strand, five prime exon/, 'error if exons are in wrong order on -ve strand';
-
-    # brac2 exons on +ve strand
-    # validate_exon_targets is called in BUILD method
-    ok my $brac2_obj = $metaclass->new_object(
-        dir              => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species          => 'Human',
-        five_prime_exon  => 'ENSE00001484009',
-        three_prime_exon => 'ENSE00003659301 ',
-        target_genes     => [ 'brac2' ],
-    ), 'can grab new object with multi exon targets';
-    ok my $brac2_exon_5p = $o->build_exon( 'ENSE00001484009' ), 'can grab exon';
-    ok my $brac2_exon_3p = $o->build_exon( 'ENSE00003659301 ' ), 'can grab exon';
-
-    ok $brac2_obj->chr_strand(1), 'set strand as 1';
-    throws_ok{
-        $brac2_obj->validate_exon_targets( $brac2_exon_3p, $brac2_exon_5p )
-    } qr/On \+ve strand, five prime exon/, 'error if exons are in wrong order on +ve strand';
-
-}
-
-sub build_exon : Test(5) {
+sub target_data : Tests(6) {
     my $test = shift;
     ok my $o = $test->_get_test_object, 'can grab test object';
 
-    throws_ok {
-        $o->build_exon( 'ENSE00002184' );
-    } qr/Unable to retrieve exon/, 'throws error for invalid exon id';
-
-    ok my $exon = $o->build_exon( 'ENSE00002184393' ), 'can retrieve exon';
-    isa_ok $exon, 'Bio::EnsEMBL::Exon';
-    is $exon->coord_system_name, 'chromosome', 'coordinate system name for exon is chromosome';
+    ok $o->target_data, 'we have target_data';
+    ok $o->have_target_data( 'target_start' ), 'we have a target_start value';
+    is $o->get_target_data( 'target_start' ), 118964564, 'target_start is correct';
+    is $o->get_target_data( 'chr_name' ), 11, 'chromosome is correct';
+    ok !$o->have_target_data('foo'), 'does not have target data foo';
 }
 
 sub check_oligo_region_sizes : Test(3) {
@@ -132,15 +37,12 @@ sub check_oligo_region_sizes : Test(3) {
 
     my $metaclass = $test->get_test_object_metaclass();
     my $new_obj2 = $metaclass->new_object(
-        dir             => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species         => 'Human',
-        five_prime_exon => 'ENSE00002184393',
-        target_genes    => [ 'test_gene' ],
+        dir              => $o->dir, 
         region_length_5F => 10,
     );
 
     throws_ok{
-        $new_obj2->get_oligo_pair_region_coordinates
+        $new_obj2->check_oligo_region_sizes
     } qr/5F region too small/
         , 'throws error if a oligo region is too small';
 }
@@ -148,15 +50,14 @@ sub check_oligo_region_sizes : Test(3) {
 sub _get_test_object {
     my ( $test, $params ) = @_;
 
-    my $metaclass = $test->get_test_object_metaclass( [ 'DesignCreate::CmdRole::OligoPairRegionsGibson' ] );
-    my $o = $metaclass->new_object(
-        dir             => tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute,
-        species         => 'Human',
-        five_prime_exon => 'ENSE00002184393',
-        target_genes    => [ 'test_gene' ],
-    );
+    my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
+    my $data_dir = dir($FindBin::Bin)->absolute->subdir('test_data/oligo_pair_regions_gibson');
 
-    return $o;
+    # need oligo target region files to test against, in oligo_target_regions dir
+    dircopy( $data_dir->stringify, $dir->stringify );
+
+    my $metaclass = $test->get_test_object_metaclass( [ 'DesignCreate::CmdRole::OligoPairRegionsGibson' ] );
+    return $metaclass->new_object( dir => $dir );
 }
 
 1;
