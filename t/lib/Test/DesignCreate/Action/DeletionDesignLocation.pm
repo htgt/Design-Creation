41ed697eb27c0ac98ec1eddde2c143b5e52ce1c7
Sajith Perera <sajith.perera@gmail.com>
2014-02-06 10:39:43 +0000

    rename tests to match command name changes

--- t/lib/Test/DesignCreate/Action/DeletionDesignLocation.pm
+++ t/lib/Test/DesignCreate/Action/DeletionDesignLocation.pm
@@ -1,4 +1,4 @@
-package Test::DesignCreate::Action::DelExonDesign;
+package Test::DesignCreate::Action::DeletionDesignLocation;
 
 use strict;
 use warnings FATAL => 'all';
@@ -9,19 +9,23 @@ use Path::Class qw( tempdir );
 use base qw( Test::DesignCreate::Class Class::Data::Inheritable );
 
 # Testing
-# DesignCreate::Action::DelExonDesign ( through command line )
+# DesignCreate::Action::DeletionDesignLocation ( through command line )
 
-sub del_exon_design_cmd : Test(4) {
+sub ins_del_design_cmd : Test(4) {
     my $test = shift;
 
     my $dir = tempdir( TMPDIR => 1, CLEANUP => 1 )->absolute;
 
     my @argv_contents = (
-        'del-exon-design'  ,
+        'deletion-design-location'  ,
         '--dir'           , $dir->stringify,
-        '--species'       , 'Human',
-        '--target-exon'   , 'ENSE00001764544',
-        '--target-gene'   , 'ATP2BF',
+        '--species'       , 'Mouse',
+        '--target-start'  , 101176328,
+        '--target-end'    , 101176428,
+        '--chromosome'    , 11,
+        '--strand'        , 1,
+        '--target-gene'   , 'LBL-TEST',
+        '--design-method' , 'deletion',
     );
 
     note('############################################');
