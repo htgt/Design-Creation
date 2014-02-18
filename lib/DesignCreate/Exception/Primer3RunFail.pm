5427b40302607c056294607c212c7e7a8823e2e9
Sajith Perera <sajith.perera@gmail.com>
2014-02-18 14:07:55 +0000

    add new exception class for primer3 run fail

---
+++ lib/DesignCreate/Exception/Primer3RunFail.pm
@@ -0,0 +1,50 @@
+package DesignCreate::Exception::Primer3RunFail;
+
+use Moose;
+use namespace::autoclean;
+
+extends qw( DesignCreate::Exception );
+
+has '+message' => (
+    default => 'Primer3 failed to run'
+);
+
+has region => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has primer3_error => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+override as_string => sub {
+    my $self = shift;
+
+    my $str = "Primer3 failed to run, error: " . $self->primer3_error;
+
+    if ( $self->show_stack_trace ) {
+        $str .= "\n\n" . $self->stack_trace->as_string;
+    }
+
+    return $str;
+};
+
+around as_hash => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $hash = $self->$orig;
+
+    $hash->{reasons} = $self->primer3_error;
+    $hash->{region} = $self->region;
+
+    return $hash;
+};
+
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
+
+1;
