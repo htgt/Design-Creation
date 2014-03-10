7a6d9a9ae070ea31af71660df30b8f3cc490c24d
Sajith Perera <sajith.perera@gmail.com>
2014-03-10 10:01:01 +0000

    update farm wrapper script

--- script/farm-wrapper-script.sh
+++ script/farm-wrapper-script.sh
@@ -11,9 +11,11 @@ mkdir -p $WORK_DIR || exit 1
 
 INPUT_FILE=$BASE_DIR/input/$LSB_JOBINDEX
 
-$PROGRAMPATH/create-multiple-designs.pl --debug --persist --del-exon --alt-designs --file $INPUT_FILE --dir $WORK_DIR
+$PROGRAMPATH/create-multiple-designs.pl --debug --persist --gibson-del-exon --file $INPUT_FILE --dir $WORK_DIR
 
 exit $?
 
 #split -a1 -d -l 380 designs.txt input/
-#~/workspace/LIMS2-WebApp/script/run_in_perlbrew 'bsub -J"create-designs[1-10]%3" -P team87-grp -q long -R"select[mem>800] rusage[mem=800]" -M800000 -o output/create-designs.%J-%I -e error/create-designs.%J-%I ~/workspace/Design-Creation/bin/farm-wrapper-script.sh'
+
+# source /nfs/team87/farm3_lims2_vms/conf/run_in_farm3 lims2_live 
+#bsub -J"create-designs[1-10]%3" -G team87-grp -q long -R"select[mem>2500] rusage[mem=2500] span[hosts=1]" -M2500 -n2 -o output/create-designs.%J-%I -e error/create-designs.%J-%I ~/workspace/Design-Creation/bin/farm-wrapper-script.sh
