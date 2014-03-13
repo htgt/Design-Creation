32596b201ec99153a179cbf703eba0256a7d7bf0
Sajith Perera <sajith.perera@gmail.com>
2014-03-13 11:31:48 +0000

    stick in subroutine that roles we are testing expects

--- t/lib/Test/ObjectRole/DesignCreate.pm
+++ t/lib/Test/ObjectRole/DesignCreate.pm
@@ -7,9 +7,18 @@ use namespace::autoclean;
 with qw(
 MooseX::Log::Log4perl
 MooseX::Getopt
-DesignCreate::Role::CmdStep
+DesignCreate::Role::Common
 DesignCreate::Role::EnsEMBL
 );
+
+=head2 update_design_attempt_record
+
+For step commands we do not update design attempt data
+
+=cut
+sub update_design_attempt_record {
+    return;
+}
 
 __PACKAGE__->meta->make_immutable;
 
