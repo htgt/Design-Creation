a8362327b85a54974a7f0cc629d8e37407056f1c
Sajith Perera <sajith.perera@gmail.com>
2014-03-17 10:30:40 +0000

    add persist attribute and fill in update design attempt code so we can test that we are firing off the right requests to the api

--- t/lib/Test/ObjectRole/DesignCreate.pm
+++ t/lib/Test/ObjectRole/DesignCreate.pm
@@ -2,6 +2,7 @@ package Test::ObjectRole::DesignCreate;
 
 use Moose;
 use Fcntl; # O_ constants
+use Try::Tiny;
 use namespace::autoclean;
 
 with qw(
@@ -11,12 +12,32 @@ DesignCreate::Role::Common
 DesignCreate::Role::EnsEMBL
 );
 
+has persist => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0
+);
+
 =head2 update_design_attempt_record
 
-For step commands we do not update design attempt data
+When running standalone 'step' commands we would not update design attempt records. However when running it as part of a 'complete' design
+create command we would attempt to update design attempt records.
+
+We leave it up to the individual tests as to if the persist flag is
+set to true or not.
 
 =cut
 sub update_design_attempt_record {
+    my ( $self, $data ) = @_;
+    return if !$self->persist;
+
+    try{
+        my $design_attempt = $self->lims2_api->PUT( 'design_attempt', $data );
+    }
+    catch {
+        $self->log->error( "Error updating design attempt record: $_" );
+    };
+
     return;
 }
 
