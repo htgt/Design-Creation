b057fdcb8bdbaa3eb67003ab88fd2e029324c6bd
Sajith Perera <sajith.perera@gmail.com>
2014-05-01 08:10:21 +0100

    add command to merge the data from the newly found G oligos with the U / D oligos of a existing design

---
+++ lib/DesignCreate/Cmd/Step/ConsolidateShortenArmDesignData.pm
@@ -0,0 +1,47 @@
+package DesignCreate::Cmd::Step::ConsolidateShortenArmDesignData;
+
+=head1 NAME
+
+DesignCreate::Cmd::Step::ConsolidateShortenArmDesignData - Bring together all the short arm design data into one file
+
+=head1 DESCRIPTION
+
+Create one yaml file containing all the data for one short arm design:
+Target
+Species
+Phase
+Design Type
+Created By
+Oligos
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use Const::Fast;
+use Try::Tiny;
+use namespace::autoclean;
+
+extends qw( DesignCreate::Cmd::Step );
+with 'DesignCreate::CmdRole::ConsolidateShortenArmDesignData';
+
+sub execute {
+    my ( $self, $opts, $args ) = @_;
+
+    try{
+        $self->consolidate_design_data;
+    }
+    catch{
+        $self->log->error( "Unable to consolidate design data:\n" . $_ );
+    };
+
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
