7acde5f7b2d8649cfd63d3a0e2d481820014d5bb
Sajith Perera <sajith.perera@gmail.com>
2015-05-20 07:44:17 +0100

    install t87 Primer3 into /opt/t87/global/ folder and point to it

--- lib/DesignCreate/Constants.pm
+++ lib/DesignCreate/Constants.pm
@@ -59,7 +59,7 @@ const our $AOS_LOCATION => $ENV{AOS_LOCATION}
     || '/nfs/team87/farm3_lims2_vms/software/AOS';
 
 const our $PRIMER3_CMD => $ENV{PRIMER3_CMD}
-    || '/nfs/users/nfs_s/sp12/workspace/primer3-2.3.5/src/primer3_core';
+    || '/opt/t87/global/software/primer3-2.3.6/src/primer3_core';
 #TODO switch to value below once vms upgraded sp12 Wed 11 Dec 2013 13:56:19 GMT
 # '/nfs/team87/farm3_lims2_vms/software/primer3/src/primer3_core'
 
